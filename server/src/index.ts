c4a1b510cac58e7b4f57b5e66eedcb712f5b9684
Hammer Guerrero <hammerg747@gmail.com>
2026-04-19 20:38:18 +0200

    Fix wildcard route per Express 5

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,7 +24,7 @@ app.use("/api/ritiri", ritiriRoutes);
 const distPath = path.resolve(__dirname, "../../dist");
 if (fs.existsSync(distPath)) {
   app.use(express.static(distPath));
-  app.get("*", (_req, res) => {
+  app.use((_req, res) => {
     res.sendFile(path.join(distPath, "index.html"));
   });
 }
